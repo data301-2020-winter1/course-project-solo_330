201a5b5dcbadf5a851f41876ae99d893341ed456
Ruben Thiemann <ruben.thiemann@gmail.com>
2020-11-11 23:36:40 +0100

    Completed Milestone2 (Task1, Task2, Task3) and updated analysis/README.md

---
+++ analysis/scripts/python_functions.py
@@ -0,0 +1,107 @@
+import pandas as pd
+#import altair as alt
+#import datetime
+#import time
+#import pdb
+import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+def blue_or_red(dataframe, variable):
+    df = dataframe
+    df['first' + variable] = np.where(df['blueFirst' + variable] == 1, 'blue', 
+                                 (np.where(df['redFirst' + variable] == 1, 'red', 'none')))
+    return df
+
+def load_and_process(url_or_path_to_csv_file):
+    
+    df = pd.read_csv(url_or_path_to_csv_file)
+    
+    count_nan = df.isnull().sum().sum()
+    if count_nan > 0:
+        print("There are {} NaN values in the dataset.".format(count_nan))
+    
+    df['winner'] = np.where(df['blueWins'] == 1, 'blue', 'red')
+    df = blue_or_red(df, 'Blood')
+    df = blue_or_red(df, 'Tower')
+    df = blue_or_red(df, 'Baron')
+    df = blue_or_red(df, 'Dragon')
+    df = blue_or_red(df, 'Inhibitor')
+    df = (df.drop(columns = ['blueWins', 
+                            'blueFirstBlood', 
+                            'blueFirstTower',
+                            'blueFirstBaron', 
+                            'blueFirstDragon', 
+                            'blueFirstInhibitor',
+                            'redWins', 
+                            'redFirstBlood', 
+                            'redFirstTower',
+                            'redFirstBaron', 
+                            'redFirstDragon', 
+                            'redFirstInhibitor'])
+          .rename(columns = {"gameId":"gameID",
+                             "gameDuraton":"gameDuration",
+                             "blueWardPlaced":"blueWard",
+                             "blueWardkills":"blueWardKills",
+                             "blueDeath":"blueDeaths",
+                             "blueAssist":"blueAssists",
+                             "blueChampionDamageDealt":"blueChampionDamage",
+                             "blueTotalHeal":"blueTotalHealing",
+                             "blueObjectDamageDealt":"blueObjectiveDamage",
+                             "blueTotalMinionKills":"blueTotalCS",
+                             "blueJungleMinionKills":"blueJungleCS",
+                             "redWardPlaced":"redWard",
+                             "redWardkills":"redWardKills",
+                             "redDeath":"redDeaths",
+                             "redAssist":"redAssists",
+                             "redChampionDamageDealt":"redChampionDamage",
+                             "redTotalHeal":"redTotalHealing",
+                             "redObjectDamageDealt":"redObjectiveDamage",
+                             "redTotalMinionKills":"redTotalCS",
+                             "redJungleMinionKills":"redJungleCS",})
+         .reindex(columns = ['gameID', 
+                             'gameDuration', 
+                             'winner', 
+                             'firstBlood', 
+                             'firstTower', 
+                             'firstDragon',
+                             'firstBaron', 
+                             'firstInhibitor',
+                             'blueTowerKills',
+                             'blueDragonKills',
+                             'blueBaronKills', 
+                             'blueInhibitorKills', 
+                             'blueWard', 
+                             'blueWardKills',
+                             'blueKills', 
+                             'blueDeaths', 
+                             'blueAssists', 
+                             'blueTotalLevel', 
+                             'blueAvgLevel',
+                             'blueChampionDamage', 
+                             'blueObjectiveDamage',
+                             'blueTotalHealing', 
+                             'blueTotalGold', 
+                             'blueTotalCS',
+                             'blueJungleCS', 
+                             'blueKillingSpree',
+                             'redTowerKills',
+                             'redDragonKills',
+                             'redBaronKills', 
+                             'redInhibitorKills', 
+                             'redWard', 
+                             'redWardKills',
+                             'redKills', 
+                             'redDeaths', 
+                             'redAssists', 
+                             'redTotalLevel', 
+                             'redAvgLevel',
+                             'redChampionDamage', 
+                             'redObjectiveDamage',
+                             'redTotalHealing', 
+                             'redTotalGold', 
+                             'redTotalCS',
+                             'redJungleCS', 
+                             'redKillingSpree']))
+
+    return df
